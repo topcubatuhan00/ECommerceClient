de5f2d08dc7c0259b4db516631498203ab9c123b
topcubatuhan00 <topcubatuhan00@gmail.com>
2024-09-12 11:45:35 +0300

    jquery & bootstrap added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
+import * as jQuery from 'jquery';
+const $ = jQuery.default;
+
 @Component({
   selector: 'app-root',
   standalone: true,
