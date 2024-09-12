d37346be712d3a53b0b55c1f16548cfb8f5ede51
topcubatuhan00 <topcubatuhan00@gmail.com>
2024-09-12 10:43:51 +0300

    admin angular materail  added

--- src/app/admin/layout/components/sidebar/sidebar.component.ts
+++ src/app/admin/layout/components/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatListModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -3,14 +3,14 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent,SidebarComponent,RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, SidebarComponent, RouterOutlet, MatSidenavModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
-
 }
